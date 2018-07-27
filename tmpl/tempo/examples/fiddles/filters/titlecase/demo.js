a62d59a03f0b48bfd175228cc4fad2f842cf0641
caizhongyi <274142680@qq.com>
2018-07-27 10:32:31 +0800

    1

--- tmpl/tempo/examples/fiddles/filters/titlecase/demo.js
+++
@@ -1,9 +0,0 @@
-var data = [
-    {'name': 'snow white and the seven dwarfs'},
-    {'name': 'the last of the mohicans'}
-];
-
-// Render
-$(document).ready( function () {
-    Tempo.prepare('list').render(data);
-});
